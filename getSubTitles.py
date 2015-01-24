371923cc95e4c8591ce7363fb3cea34677b3f779
Ken Edwards <ken@kje.ca>
2015-01-24 13:18:52 -0500

    cleaning up file sturcutre, debug writing all images to a directory

--- getSubTitles.py
+++ getSubTitles.py
@@ -1,24 +1,42 @@
-
-
 import sys
 import PIL.Image
 sys.modules['Image'] = PIL.Image
-
 import Image
-
-
 import cv2
 import pytesseract
 import matplotlib.pyplot as plt
+import subprocess
+import os
 
-# open the video
-vid = cv2.VideoCapture('/Users/ken/code/SullyText/video.mp4')
+#### RUNTIME PARAMETERS ####
+
 
 # this is where the subtitles start to come in 5730
 startFrame = 5730 
-#startFrame = 7500
+#startFrame = 67000
 
-skipFrames = 5
+# number of frames to skip each time
+skipFrames = 5 
+
+# show the movie and plot metrics or not (much faster when not)
+showGUI = 1 ; 
+
+
+# miminum different number of pixels between frames of text to 
+# declare that we have new text
+diffValMin = 2000  
+
+# minumum number of non-zero pixels we need in text frame 
+# to declare that we have something to decode
+nonZeroMin = 100 
+
+
+
+
+
+
+# open the video
+vid = cv2.VideoCapture('/Users/ken/code/SullyText/video.mp4')
 
 # skip to that frame
 vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, startFrame); 
@@ -40,9 +58,14 @@ plt.ion()
 
 firstTime = 1;
 
-showGUI = 0 ; 
 
 numFrames = vid.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT);    
+
+
+c= 'rm /Users/ken/code/SullyText/textImages/*.png' 
+print 'Command is', c
+subprocess.call(c,shell=True)
+
 while res:
     curFrame = int(vid.get(cv2.cv.CV_CAP_PROP_POS_FRAMES));     
 
@@ -58,7 +81,7 @@ while res:
     # note: image is 360,640
 
     # extract the bottom part with text in it
-    textImage = image[277:360,:]
+    textImage = image[278:360,:]
 
     # display the window
     #cv2.imshow('text',textImage)
@@ -88,31 +111,38 @@ while res:
     diffVal = cv2.countNonZero(diffInverted)
     diffValList.append(diffVal)
 
-    # miminum different number of pixels between frames of text to 
-    # declare that we have new text
-    diffValMin = 2000  
+   
 
-    # minumum number of non-zero pixels we need in text frame 
-    # to declare that we have something to decode
-    nonZeroMin = 100 
-    
     # if the differences between the text frames
     if (diffVal > diffValMin and nonZero > nonZeroMin) or firstTime  :
         
+        
+        imageFileName = "textImages/%06d.png" % (curFrame,)
+        #textFileName = "textImages/%06d.txt" % (curFrame,)
+        
         # save the text image to disk
-        cv2.imwrite('out.png',inverted)
+        cv2.imwrite(imageFileName,inverted)
+
 
         # run the character recognition on it
-        curText = pytesseract.image_to_string(Image.open('out.png'))
+        curText = pytesseract.image_to_string(Image.open(imageFileName))
         
+        #delete the file we wrote, comment out if we want to debug
+        os.remove(imageFileName)
+
         # replace all newlines with spaces
         curText = curText.replace('\n',' ')
 
         allText = allText + ' ' + curText 
         print curText
+        
         text_file = open("transcript.txt", "w")
         text_file.write("%s" % allText)
         text_file.close()
+        
+        #text_file = open(textFileName, "w")
+        #text_file.write("%s" % curText)
+        #text_file.close()
     
     lastInverted = inverted.copy()
 
@@ -135,8 +165,10 @@ while res:
     if key  == 27:
         print "exiting!"
         break
-    res,image = vid.read()
+    
     vid.set(cv2.cv.CV_CAP_PROP_POS_FRAMES, curFrame + skipFrames); 
+    
+    res,image = vid.read()
     firstTime = 0 ; 
 
 
